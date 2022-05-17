21e48fcf9d45f72a43bfdeae64d8464dbc942649
ninmando <maxim.woelk@campus.tu-berlin.de>
2022-05-17 16:23:26 +0200

    basis struktur

---
+++ src/InboxQueue.java
@@ -0,0 +1,13 @@
+public class InboxQueue  implements Runnable{
+
+    public InboxQueue(){
+
+    }
+
+
+    //TODO:
+    @Override
+    public void run() {
+
+    }
+}
--- src/MessageSequencer.java
+++ src/MessageSequencer.java
@@ -1,3 +1,33 @@
-public class MessageSequencer {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class MessageSequencer implements Runnable{
+
+
+    private Queue<Message> messages;
+
+    public MessageSequencer(){
+        messages = new LinkedList<>();
+
+    }
+
+    //TODO:
+    //message sequencer continuously polls the inbox queues of all the threads
+    public int poll(){
+        return 0;
+    }
+
+
+
+    //TODO:
+    //message sequencer sends all messages to all other threads
+    public int forward(){
+        return 0;
+    }
+
+    //TODO:
+    @Override
+    public void run() {
+
+    }
 }
